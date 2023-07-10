a5681813d90b717136c6009c3b4612152bb9587c
StigvarBarAbbas <136749616+StigvarBarAbbas@users.noreply.github.com>
2023-07-10 14:36:31 -0300

    Update Entrega3(1).java

--- Entrega3(1).java
+++ Entrega3(1).java
@@ -6,32 +6,32 @@ class Entrega3 {
 	int a, b, c, p, r;
 	Scanner ent = new Scanner(System.in);
 	do {
-		System.out.println("Vamos medir a área e o perímetro do polígono!");
-	System.out.println("Informe um número positivo para o lado A");
+		System.out.println("Vamos medir a Ã¡rea e o perÃ­metro do polÃ­gono!");
+	System.out.println("Informe um nÃºmero positivo para o lado A");
 	a = ent.nextInt();
 	if (a<0) {
 		a=a*-1;
-		System.out.println("Estamos medindo para o lado contrário, entendi.");
+		System.out.println("Estamos medindo para o lado contrÃ¡rio, entendi.");
 	}
-	System.out.println("Informe um número positivo para o lado B");
+	System.out.println("Informe um nÃºmero positivo para o lado B");
 	b = ent.nextInt();
 	if (b<0) {
 		b=b*-1;
-		System.out.println("Estamos medindo para o lado contrário, entendi.");
+		System.out.println("Estamos medindo para o lado contrÃ¡rio, entendi.");
 	}
 	while (a==0|b==0) {
-		System.out.println("Nós precisamos de valores válidos para medir o polígono");
-		System.out.println("Informe um número positivo para o lado A");
+		System.out.println("NÃ³s precisamos de valores vÃ¡lidos para medir o polÃ­gono");
+		System.out.println("Informe um nÃºmero positivo para o lado A");
 		a = ent.nextInt();
-		System.out.println("Informe um número positivo para o lado B");
+		System.out.println("Informe um nÃºmero positivo para o lado B");
 		b = ent.nextInt();
 	}
 	c= a*b;
 	p= (2*a)+(2*b);
 	if(a==b){
-		System.out.println("O quadrado de lados "+a+" possui área de "+c+"u² e perímetro de "+p+"u");
+		System.out.println("O quadrado de lados "+a+" possui Ã¡rea de "+c+"uÂ² e perÃ­metro de "+p+"u");
 	}else {
-		System.out.println("O retângulo de lado A= " + a + " e lado B= " + b + " possui área de " + c + "u² e perímetro de " + p + "u");
+		System.out.println("O retÃ¢ngulo de lado A= " + a + " e lado B= " + b + " possui Ã¡rea de " + c + "uÂ² e perÃ­metro de " + p + "u");
 	}
 	System.out.println("Deseja calcular novamente? Digite 1 para sim e 2 para encerrar.");
 	r = ent.nextInt();
